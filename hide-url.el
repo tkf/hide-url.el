f9c47c5cbf917b6cd31d29035396cfb73898f38a
Takafumi Arakaki <aka.tkf@gmail.com>
2011-12-16 22:02:03 +0100

    Added hide-url/{show,hide}-all and their helpers

--- hide-url.el
+++ hide-url.el
@@ -32,6 +32,50 @@
 
 ;;; Code:
 
+(defvar hide-url/offset-beg 10)
+(defvar hide-url/offset-end 10)
 
+(defun hide-url/hide-all ()
+  "Hide all URLs"
+  (interactive)
+  (with-silent-modifications
+    (save-excursion
+      (beginning-of-buffer)
+      (while (search-forward-regexp thing-at-point-url-regexp nil t)
+        (hide-url/hide-at-point)))))
+
+(defun hide-url/show-all ()
+  "Show all hidden URLs"
+  (interactive)
+  (with-silent-modifications
+    (let ((beg) (end (point-min)))
+      (while end
+        (setq beg (next-single-property-change end 'hide-url/hidden))
+        (setq end (when beg
+                    (next-single-property-change beg 'hide-url/hidden)))
+        (when end
+          (hide-url/remove-text-properties beg end))))))
+
+(defun hide-url/hide-at-point ()
+  "Replace URL at point with something like 'https://gi....ide-url.el'"
+  (let ((bounds (thing-at-point-bounds-of-url-at-point)))
+    (when bounds
+      (let ((hide-beg (+ (car bounds) hide-url/offset-beg))
+            (hide-end (- (cdr bounds) hide-url/offset-end)))
+        (when (< hide-beg hide-end)
+          (hide-url/put-text-properties hide-beg hide-end))))))
+
+(defun hide-url/put-text-properties (beg end)
+  (add-text-properties
+   beg end
+   '(display
+     "..."
+     hide-url/hidden
+     t)))
+
+(defun hide-url/remove-text-properties (beg end)
+  (remove-list-of-text-properties
+   beg end
+   '(display hide-url/hidden)))
 
 ;;; hide-url.el ends here
